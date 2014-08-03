d229ab722b3d350b23f4178b932115a9d0851759
Karan Sikka <kssworld93@gmail.com>
2014-08-03 11:44:17 -0400

    good data

--- data_gen.py
+++ data_gen.py
@@ -24,6 +24,42 @@ def get_all_data(github, username):
             }
     return data
 
+def repo_summary(userdata, repo, repo_contributors, repo_commits):
+    """
+    IN
+    userdata : dict
+    repo : dict
+    repo_contributors : list of dicts
+    repo_commits : list of dicts
+    """
+    username = userdata['login']
+
+    # map from username to number of commits.
+    # useful for contribution pie chart.
+    contrib_dist = { c['author']['login'] : c['total'] for c in repo_contributors }
+
+    # date of most recent commit as a string in ISO fmt.
+    # useful to put repos in chronological order for the user
+    last_commit_date = None
+    for commit in repo_commits:
+        if commit['author'] is None:
+            # github did not recognize the author of this commit.
+            # check if name matches the github name.
+            if commit['commit']['author']['name'].lower() == userdata['name'].lower():
+                last_commit_date = commit['commit']['author']['date']
+                break
+        elif commit['author']['login'] == username:
+            last_commit_date = commit['commit']['author']['date']
+            break
+
+    data = {'contrib_dist': contrib_dist,
+            'last_commit_date': last_commit_date,
+            }
+
+    return data
+
+
+
 if __name__ == '__main__':
     """
     Note: this will only work after
@@ -32,6 +68,17 @@ if __name__ == '__main__':
     """
     from githubgetter import GitHubAgent as GitHub
     github = GitHub(None)
-    data = get_all_data(github, 'ksikka')
+    username = 'ksikka'
+
+    data = get_all_data(github, username)
     with open('sample_data.json', 'w') as f:
         json.dump(data, f, indent=2)
+
+    userdata = data['userdata']
+    summaries = {}
+    for repo in data['repos']:
+        repo_name = repo['full_name']
+        repo_contributors = data['stats'][repo_name]
+        repo_commits = data['commits'][repo_name]
+        summaries[repo_name] = repo_summary(userdata, repo, repo_contributors, repo_commits)
+    print json.dumps(summaries, indent=4)
